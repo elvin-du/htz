b0fee0d3e120a94d32d76e4d3013cd1e662efaa9
nick Du <nick@yun.io>
2015-05-20 15:03:49 +0800

    update layout

--- controllers/nav.go
+++ controllers/nav.go
@@ -1,32 +1,32 @@
 package controllers
 
-import (
-	"github.com/astaxie/beego"
-	"htz/models"
-	"strconv"
-)
+//import (
+//	"github.com/astaxie/beego"
+//	"htz/models"
+//	"strconv"
+//)
 
 type NavController struct {
 	Controller
 }
 
 func (this *NavController) Get() {
-	idStr := this.Ctx.Input.Param(":id")
-	id, err := strconv.Atoi(idStr)
-	if nil != err {
-		beego.Error(err)
-        this.Abort(err.Error())
-		return
-	}
+//	idStr := this.Ctx.Input.Param(":id")
+//	id, err := strconv.Atoi(idStr)
+//	if nil != err {
+//		beego.Error(err)
+//        this.Abort(err.Error())
+//		return
+//	}
 
-	article, err := models.NavModel().InfoById(id)
-	if nil != err {
-		beego.Error(err)
-		this.Abort(err.Error())
-		return
-	}
+//	article, err := models.NavModel().InfoById(id)
+//	if nil != err {
+//		beego.Error(err)
+//		this.Abort(err.Error())
+//		return
+//	}
 
-	this.Data["Article"] = article
-	this.Layout = "index.html"
-	this.TplNames = "articles.html"
+//	this.Data["Article"] = article
+//	this.Layout = "index.html"
+//	this.TplNames = "articles.html"
 }
